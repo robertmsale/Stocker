1cb932ee3bfb4a808b5ab0bb9061f0602407a710
Robert Sale <robert.sale@outlook.com>
2023-01-15 13:51:49 -0800

    Initial Commit

---
+++ Stocker/StockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockerApp.swift
+//  Stocker
+//
+//  Created by Robert Sale on 1/15/23.
+//
+
+import SwiftUI
+
+@main
+struct StockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
