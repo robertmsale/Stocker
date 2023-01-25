b874518194fdfe374b887a44b1dc5225314d7f9f
Robert Sale <robert.sale@outlook.com>
2023-01-25 14:13:34 -0800

    Scaffolding

---
+++ Stocker/ScanQRView.swift
@@ -0,0 +1,20 @@
+//
+//  ScanQRView.swift
+//  Stocker
+//
+//  Created by Robert Sale on 1/19/23.
+//
+
+import SwiftUI
+
+struct ScanQRView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScanQRView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScanQRView()
+    }
+}
